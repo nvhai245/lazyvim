3c0a14087b32199949cfc2a44875613644ddc829
Hai Nguyen <nvhai245@gmail.com>
2025-05-16 19:03:24 +0700

    updated color scheme

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,25 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-vim.cmd([[
-hi Normal guibg=NONE ctermbg=NONE
-hi NormalNC guibg=NONE ctermbg=NONE
-hi SignColumn guibg=NONE ctermbg=NONE
-hi StatusLine guibg=NONE ctermbg=NONE
-hi StatusLineNC guibg=NONE ctermbg=NONE
-hi VertSplit guibg=NONE ctermbg=NONE
-hi TabLine guibg=NONE ctermbg=NONE
-hi TabLineFill guibg=NONE ctermbg=NONE
-hi TabLineSel guibg=NONE ctermbg=NONE
-hi Pmenu guibg=NONE ctermbg=NONE
-hi PmenuSel guibg=NONE ctermbg=NONE
-hi EndOfBuffer guibg=NONE ctermbg=NONE
-hi SnacksPicker guibg=NONE ctermbg=NONE
-]])
+vim.api.nvim_set_hl(0, "SnacksDashboardHeader", { fg = "#eeeeee" })
+vim.api.nvim_set_hl(0, "SnacksDashboardKey", { fg = "#eeeeee" })
 
 -- autosave
 local autocmd = vim.api.nvim_create_autocmd
 autocmd({ "CmdlineEnter", "TabLeave", "TermOpen", "FocusLost", "TabNew", "WinNew" }, {
   pattern = "*",
   command = "wall",
+})
+
+-- open snacks explorer
+autocmd({ "SessionLoadPost" }, {
+  pattern = "*",
+  command = "lua require('snacks').explorer.open({focus = false})",
 })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,32 +1,26 @@
 return {
   {
-    "zenbones-theme/zenbones.nvim",
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    dependencies = "rktjmp/lush.nvim",
-    -- you can set set configuration options here
+    "slugbyte/lackluster.nvim",
     config = function()
-      vim.g.zenbones_darkness = "stark"
-      vim.g.zenbones_transparent_background = true
+      require("lackluster").setup({
+        disable_plugin = {
+          bufferline = true,
+        },
+      })
     end,
   },
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function(_, opts)
-      local custom_zenbones = require("lualine.themes.zenbones")
       -- Change the background of lualine sections
-      custom_zenbones.normal.a.bg = "NONE"
-      custom_zenbones.normal.b.bg = "NONE"
-      custom_zenbones.normal.c.bg = "NONE"
-      table.insert(opts.options, { theme = "zenbones" })
+      table.insert(opts.options, { theme = "lackluster" })
     end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "zenbones",
+      colorscheme = "lackluster-night",
     },
   },
 }
