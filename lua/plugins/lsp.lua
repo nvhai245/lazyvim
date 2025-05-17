0ea4874281c04229110fd8807fe22358d11369f3
Hai Nguyen <nvhai245@gmail.com>
2025-05-18 01:45:04 +0700

    added lsp, treesitter

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,10 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      -- will be automatically installed with mason and loaded with lspconfig
+      htmx = {},
+      superhtml = {},
+    },
+  },
+}
